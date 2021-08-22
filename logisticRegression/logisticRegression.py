8259d1a2c511c73095bb0dcdd2593a6343b0ced4
ppinko <p.pinkowicz@gmail.com>
2021-08-22 11:07:44 +0200

    IMPLEMENT - Add hypthoesis and cost function for logistic regression

--- logisticRegression/logisticRegression.py
+++ logisticRegression/logisticRegression.py
@@ -19,6 +19,49 @@ def sigmoid(Z):
     return sig
 
 
+def hypothesisLogisticRegression(X, theta):
+    """
+    Calculates hypthothesis for logistic regression.
+
+    Equation:
+    h = g(X * theta)
+    where:
+        g(z) = 1 / (1 + e^(-z)) # sigmoid function
+
+    m - size of training set
+    n - number of features (including feature zero - 'bias')
+
+    X - the independent variables (features) (size m x n)
+    theta - the coefficent for the features (size n x 1)
+
+    return - value based on the hyphothesis
+    """
+    temp = np.dot(X, theta)
+    return sigmoid(temp)
+
+
+def costLogisticRegression(Y, X, theta):
+    """
+    Cost function for logistic regression with multiple features.
+
+    Equation:
+    cost = (1 / m) * sum((-Y * log(h))-((1 - Y)* log(1 - h)))
+
+    m - size of training set
+    n - number of features (including feature zero - 'bias')
+
+    Y - output, target variable (size m x 1)
+    X - the independent variables (features) (size m x n)
+    theta - the coefficent for the features (size n x 1)
+
+    return - cost for given theta and bias
+    """
+    m = len(Y)
+    leftTrue = -Y * np.log(hypothesisLogisticRegression(X, theta))
+    rightFalse = -(1-Y) * np.log(1 - hypothesisLogisticRegression(X, theta))
+    return (1 / m) * np.add(leftTrue, rightFalse).sum()
+
+
 if __name__ == "__main__":
     # test sigmoid funciton
     arr = np.array([[-1, 0, 1]])

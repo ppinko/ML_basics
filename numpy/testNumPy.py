d356835d13bc77b0e0e54324d0431b37b46bc8c4
ppinko <p.pinkowicz@gmail.com>
2021-09-17 16:25:48 +0200

    IMPLEMENT - Extend notes by adding rotation and assignment of submatrix

--- numpy/testNumPy.py
+++ numpy/testNumPy.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 A = np.array([[1, 2, 3], [4, 5, 6]], dtype=int)
+print(type(A))
 B = np.array([[1, 2], [3, 4], [5, 6]])
 print('Size of A = {0}'.format(A.shape))
 print('Size of B = {0}'.format(B.shape))
@@ -29,3 +30,27 @@ print(F3)
 G = np.array([[1, 2, 3]])
 print(G)
 print(G.shape)
+print('\n')
+
+# column wise multiplication
+M1 = np.array([[1, 2], [3, 4]])
+M2 = np.column_stack((M1, np.multiply(M1[:, 0], M1[:, 1])))
+print(M1)
+print(M2)
+
+# test rotation
+print('/nRotation of the np.array:')
+R1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print('Before transformation: \n {0}'.format(R1))
+R2 = np.rot90(R1, k=1)
+print('After (left) transformation: \n {0}'.format(R2))
+R3 = np.rot90(R1, k=-1)
+print('After (right) transformation: \n {0}\n'.format(R3))
+
+# assignment to a part of the matrix
+print('\nAssignment to a part of the matrix:')
+A1 = np.array([[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 10], [10, 11, 12, 13]])
+print('Before assignment:\n{0}'.format(A1))
+zeros = np.zeros((2, 2), dtype=int)
+A1[1:3, 1:3] = zeros
+print(A1)
